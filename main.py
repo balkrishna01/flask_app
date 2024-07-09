795a5104c11e91d24ab9083b03020c3728d9c5dd
Balkrishna Oli <balkrishna.oli@veribom.com>
2024-07-09 11:56:05 +0545

    added get students route

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# let's import the flask
+from flask import Flask, Response
+import json
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 import pymongo
-from flask import Flask, render_template
-import os  # importing operating system module
+import os
+
+
+app = Flask(__name__)
 
 # mongodb://mongo:mongo@localhost:27017/
 MONGODB_URI = 'mongodb://mongo:mongo@localhost:27017/'
@@ -10,11 +15,12 @@ client = pymongo.MongoClient(MONGODB_URI)
 # Creating database
 db = client.my_mongo_db
 
-# Creating students collection and inserting a document
-db.students.insert_one({'name': 'Peter', 'country': 'Nepal', 'city': 'Kathmandu', 'age': 200})
-print(client.list_database_names())
 
-app = Flask(__name__)
+@app.route('/api/v1/students', methods=['GET'])
+def get_students():
+    students = db.students.find()
+    return Response(dumps(students), mimetype='application/json')
+
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
